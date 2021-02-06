b42644697da1ab54c4f5fb5706115417c418c6d9
Christine Fong <christinefongwl@gmail.com>
2021-02-06 21:15:30 +0800

    deleted some comments

--- src/components/ChooseList.js
+++ src/components/ChooseList.js
@@ -1,13 +1,9 @@
-// import { useState } from 'react';
-
 const ChooseList = ( {onSelect} ) => {
-  // const [list, setList] = useState('All');
 
   return (
     <select 
       name='choose-list' 
       className='choose-list' 
-      // value={e.target.value} 
       onChange={(e) => onSelect(e.target.value)}
     >
       <option value='all'>All</option>
